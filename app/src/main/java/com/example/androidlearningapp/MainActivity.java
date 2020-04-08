0bdd3d511190396ca9e826d861ed57c8f71d1f7f
Loganathan Sellappa <loganathan.sellappa@logmein.com>
2020-04-08 20:38:54 +0200

    Switch image onclick

--- app/src/main/java/com/example/androidlearningapp/MainActivity.java
+++ app/src/main/java/com/example/androidlearningapp/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.androidlearningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void switchImage(View view) {
+        ImageView image = (ImageView) findViewById(R.id.imageView2);
+        image.setImageResource(R.drawable.cutebaby1);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
