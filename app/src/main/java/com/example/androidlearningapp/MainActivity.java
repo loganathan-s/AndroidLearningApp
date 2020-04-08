3032db4b7d0aee8275daea946c224f65689e862e
Loganathan Sellappa <loganathan.sellappa@logmein.com>
2020-04-08 21:26:08 +0200

    Simple USD - Euro Converter

--- app/src/main/java/com/example/androidlearningapp/MainActivity.java
+++ app/src/main/java/com/example/androidlearningapp/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.androidlearningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertToEuro(View view) {
+        EditText dollar = (EditText) findViewById(R.id.editText);
+        Double euro = Double.valueOf(String.valueOf(dollar.getText())) * 0.92;
+        dollar.setText("");
+        Toast.makeText(this, String.format("%.2f", euro)+"€",Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
