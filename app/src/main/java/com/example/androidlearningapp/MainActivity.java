8885a37ec45f53937be62611dd3f488506518f7d
Loganathan Sellappa <loganathan.sellappa@logmein.com>
2020-04-08 20:03:56 +0200

    Sample Form to read inputs

--- app/src/main/java/com/example/androidlearningapp/MainActivity.java
+++ app/src/main/java/com/example/androidlearningapp/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.androidlearningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void validateCredentials(View view) {
+        EditText name = (EditText) findViewById(R.id.editName);
+        EditText password = (EditText) findViewById(R.id.editPassword);
+        Log.i("Entered Name is -> ", name.getText().toString());
+        Log.i("Entered Password is -> ", password.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
