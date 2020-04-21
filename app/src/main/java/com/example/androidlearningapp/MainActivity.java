9c9418101e56b2d6d93d1ae75caffd05b5ca5015
Loganathan Sellappa <loganathan.sellappa@logmein.com>
2020-04-21 17:36:41 +0200

    Shared Preference added

--- app/src/main/java/com/example/androidlearningapp/MainActivity.java
+++ app/src/main/java/com/example/androidlearningapp/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.androidlearningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.logging.SocketHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.androidlearningapp", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("testString", "TEST").apply();
+        Log.i("SharedPreferences", sharedPreferences.getString("testString", ""));
     }
 }
